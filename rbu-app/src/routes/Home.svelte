5ccc8a2cc34b77880316b72c933f514f69ccc8b6
derauld <derauld@ilcoud.com>
2021-11-10 17:08:57 +0800

    Genesis

---
+++ rbu-app/src/routes/Home.svelte
@@ -0,0 +1,16 @@
+<div class="container">	
+	<div class="content">
+		<h1>Retirement and Benefits Calculator</h1>
+
+		
+	</div>
+	<footer class="footer">
+		<div class="content has-text-centered is-size-7">
+		  <p>
+			<strong>Caclculator App </strong> by <a href="https://www.facebook.com/francischarl.samson">CSUPT FRANCIS CHARL T SAMSON</a>. The source code is licensed
+			<a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+			is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+		  </p>
+		</div>
+	  </footer>
+</div>
